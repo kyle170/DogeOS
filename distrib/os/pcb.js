1b816859ae35d52789a966ba4904e6eb70f13cca
kyle170 <kyle@gavalchin.com>
2016-10-18 18:21:06 -0400

    PCB Redo
    Needs some optimzations

--- distrib/os/pcb.js
+++ distrib/os/pcb.js
@@ -1,26 +1,8 @@
 var TSOS;
 (function (TSOS) {
     var PCB = (function () {
-        function PCB(priority) {
-            this.priority = priority;
-            this.processID = PCB.currentProcessId++;
-            this.acc = 0;
-            this.XRegister = 0;
-            this.YRegister = 0;
-            this.ZFlag = 0;
-            this.programCounter = 0;
-            this.processState = "NEW";
-            this.baseRegister = -1;
-            this.limitRegister = -1;
+        function PCB() {
         }
-        PCB.prototype.update = function (pc, Acc, XReg, YReg, Zflag) {
-            this.programCounter = pc;
-            this.acc = Acc;
-            this.XRegister = XReg;
-            this.YRegister = YReg;
-            this.ZFlag = Zflag;
-        };
-        PCB.currentProcessId = 0; // want this globally accessable... start with 0
         return PCB;
     }());
     TSOS.PCB = PCB;
--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -1,38 +1,5 @@
 module TSOS {
     export class PCB {
-		// allow these to be accessed by other class methods
-        public priority: number;           	// Importance (remeber that lower is better!)
-        public processID: number;           // Int (PID, its how we identify them)
-        public acc: number;           		// Number from 0-255
-        public XRegister: number;           // Number from 0-255
-        public YRegister: number;           // Number from 0-255
-        public ZFlag: number;           	// 0 or 1
-        public programCounter: number;      // Location of current program execution
-        public processState: string; 		//'NEW', 'READY', 'WAITING', 'HALTED', 'RUNNING', 'TERMINATED'
-        public baseRegister: number;        // Where memory access starts
-        public limitRegister: number;       // Where memory access ends
-        
-        static currentProcessId: number = 0;	// want this globally accessable... start with 0
 		
-        constructor(priority: number){	// things we need to start with Invoke on initial load
-			this.priority = priority;
-			this.processID = PCB.currentProcessId++;
-			this.acc = 0;
-			this.XRegister = 0;
-			this.YRegister = 0; 
-			this.ZFlag = 0;
-			this.programCounter = 0;
-			this.processState = "NEW";
-			this.baseRegister = -1;
-			this.limitRegister = -1;
-        } 
-		
-        public update( pc: number, Acc: number, XReg: number, YReg: number, Zflag: number ): void { // give us the old update... err new one!
-            this.programCounter = pc;
-            this.acc = Acc;
-            this.XRegister = XReg;
-            this.YRegister = YReg;
-            this.ZFlag = Zflag;
-        }
     } 
 }
