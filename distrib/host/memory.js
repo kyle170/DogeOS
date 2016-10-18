803c28b241c27a7c39889e2fa9420000e6546901
kyle170 <kyle@gavalchin.com>
2016-10-18 18:32:02 -0400

    Memory methods
    init, get, set, and reporting to dashboard

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -1,8 +1,26 @@
 var TSOS;
 (function (TSOS) {
     var Memory = (function () {
-        function Memory() {
+        function Memory(size) {
+            this.memory = new Array(size); // variable read from kernel to set memory size when the OS boots
         }
+        Memory.prototype.init = function () {
+            //when the memory is created... go through it and set all values at 00 (blank)
+            for (var i = 0; i < this.memory.length; i++) {
+                this.memory[i] = '00';
+            }
+        };
+        Memory.prototype.getByte = function (memLoc) {
+        };
+        Memory.prototype.setByte = function (memLoc, dataToSet) {
+        };
+        Memory.prototype.toString = function () {
+            var output = "";
+            for (var i = 0; i < this.memory.length; i++) {
+                output += this.memory[i] + ' ';
+            }
+            return output;
+        };
         return Memory;
     }());
     TSOS.Memory = Memory;
--- source/host/memory.ts
+++ source/host/memory.ts
@@ -1,5 +1,33 @@
 module TSOS {
     export class Memory {
-    
+		private memory;
+		
+		constructor(size: number){
+			this.memory = new Array(size); // variable read from kernel to set memory size when the OS boots
+		}
+		
+		public init(){ 
+			//when the memory is created... go through it and set all values at 00 (blank)
+			for(var i:number=0; i<this.memory.length; i++){
+				this.memory[i] = '00'; 
+			}
+		}
+		
+		public getByte(memLoc: number){
+			
+		}
+		
+		public setByte(memLoc: number, dataToSet: string){
+			
+		}
+		
+		public toString(): string{
+			var output: string = "";
+			for (var i: number=0; i<this.memory.length; i++){
+				output += this.memory[i]+' ';
+			}
+			return output;
+		}
+		
     }
 }
