3eab3b567439a4f2797922259647505a4c0fd0d6
kyle170 <kyle@gavalchin.com>
2016-10-18 19:21:55 -0400

    ProcessManager Rework
    created the methods

--- source/os/processManager.ts
+++ source/os/processManager.ts
@@ -1,5 +1,25 @@
 module TSOS {
     export class ProcessManager {
 		
+		private processes;
+		
+		constructor(){
+			// nothing here yet
+		}
+		
+		public init(){
+			//creates a blank table to start with
+			this.processes = [null];
+		}
+		
+		public load(programData: Array<String>): number {
+			//load the program into the memory and return a PID to work with
+		
+		}
+		
+		public getProcessControlBlock(ProcessID: number){
+			//simply just grabs the process control block PID
+						
+		}
     }
 }
